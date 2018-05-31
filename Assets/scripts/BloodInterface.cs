36866f082b6fee41ab2c8f29afd58ecaa5ff4504
talsharb488 <talsharb@walla.com>
2018-05-31 16:01:13 +0300

    'commiting'

---
+++ Assets/scripts/BloodInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vamp_bood_api;
+
+namespace vamp_api
+{
+    interface BloodInterface{
+
+        Boolean isAccepting(BLOOD_TYPE host, BLOOD_TYPE recieving);
+        Boolean isRejecting(BLOOD_TYPE host, BLOOD_TYPE recieving);
+);
+
+
+
+    }
