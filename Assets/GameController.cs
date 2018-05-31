402da047d3a3ea2a825851c192c01beb2b3d0e32
unknown <erezg3@gmail.com>
2018-05-31 14:54:22 +0300

    add skelton for gameController.cs

---
+++ Assets/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+    private int dayLight; //timer till end of level
+    private int score;
+    private SceneManager smanager;
+       
+    //victumWaveSpown;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
