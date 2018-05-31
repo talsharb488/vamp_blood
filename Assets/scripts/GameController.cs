e4882046e322095be96adc1d9e9fde0fd8a0502e
unknown <erezg3@gmail.com>
2018-05-31 16:25:26 +0300

    gameController has is_game_over property

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -7,16 +7,23 @@ public class GameController : MonoBehaviour {
     private int dayLight; //timer till end of level
     private int score;
     private SceneManager smanager;
+    private bool is_game_over = false;
+    public bool isGameOver {
+        get { return is_game_over; }
+    }
        
     //victumWaveSpown;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        dayLight = 100;
+        is_game_over = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        if (dayLight==0)
+            is_game_over = true;
 		
 	}
 }
